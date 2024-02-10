1b05968ffb1b44b8459b36d0a8c80d0f4ef09b58
annalelsss <frozen001004@gmail.com>
2024-02-10 22:07:56 +0900

    todo application complete!

--- todoApp/src/components/Form.jsx
+++ todoApp/src/components/Form.jsx
@@ -2,21 +2,22 @@ import { useState } from "react";
 import styles from "./form.module.css";
 
 export default function Form({ todos, setTodos }) {
-  const [todo, setTodo] = useState("");
+  // const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({ name: "", done: false });
 
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
-    setTodo("");
+    setTodo({ name: "", done: false });
   }
   return (
     <form className={styles.todoform} onSubmit={handleSubmit}>
       <div className={styles.inputContainer}>
         <input
           className={styles.todoinput}
-          onChange={(e) => setTodo(e.target.value)}
+          onChange={(e) => setTodo({ name: e.target.value, done: false })}
           type="text"
-          value={todo}
+          value={todo.name}
           placeholder="Enter todo items"
         ></input>
         <button className={styles.todobutton} type="submit">
--- todoApp/src/components/TodoItem.jsx
+++ todoApp/src/components/TodoItem.jsx
@@ -1,15 +1,27 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
-  function handleDelete() {
-    console.log("delete!");
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo.name !== item));
   }
+  function handleClick(item) {
+    const newArray = todos.map((todo) =>
+      todo.name === item ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+  }
+  const status = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={status} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
-          <button className={styles.deleteButton} onClick={handleDelete}>
+          <button
+            className={styles.deleteButton}
+            onClick={() => handleDelete(item.name)}
+          >
             Cancled
           </button>
         </span>
